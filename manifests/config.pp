6201bca9a996125f8ed6e3efcc65feb20b09a24d
Jordi Prats <jordi.prats@gmail.com>
2018-08-23 14:22:23 +0200

    bugfix

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,6 +10,6 @@ class syslogng::config inherits syslogng {
   concat::fragment{ "${syslogng::params::syslogngconf} baseconf":
     target  => $syslogng::params::syslogngconf,
     order   => '01',
-    content => template("${module_name}/${template_name}.erb"),
+    content => template("${module_name}/${syslogng::params::template_name}.erb"),
   }
 }
