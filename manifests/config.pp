85bae0c107cf4b4cf65e5d42bfb9bd7c9f2a6ef7
Jordi Prats <jordi.prats@gmail.com>
2018-08-23 11:15:45 +0200

    neteja funcionalitat extranya

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,6 +10,6 @@ class syslogng::config inherits syslogng {
   concat::fragment{ "${syslogng::params::syslogngconf} baseconf":
     target  => $syslogng::params::syslogngconf,
     order   => '01',
-    content => template("$module_name/syslogngconf.erb"),
+    content => template("${module_name}/syslogngconf.erb"),
   }
 }
--- manifests/log.pp
+++ manifests/log.pp
@@ -7,7 +7,7 @@ define syslogng::log(
   validate_array($sourcelist)
   validate_array($destinationlist)
 
-  concat::fragment{ "${syslogng::params::syslogngconf} log $name":
+  concat::fragment{ "${syslogng::params::syslogngconf} log ${name}":
     target  => $syslogng::params::syslogngconf,
     order   => '94',
     content => template("${module_name}/log.erb"),
